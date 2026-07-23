28984ecddf1ce17822f63e5ea9b2d860faadb99d
Chirag <shrivastavachirag22@gmail.com>
2026-07-23 16:29:55 +0530

    Fix footer email wrapping

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -57,7 +57,10 @@ export default function Footer() {
             <h4 className="mb-3 font-semibold text-white">Contact</h4>
             <ul className="space-y-1.5 text-sm text-[#c8dcc8]">
               <li>+61 493 106 437</li>
-              <li>healwellnaturaltherapies@gmail.com</li>
+          
+              <li className="break-all leading-relaxed">
+  healwellnaturaltherapies@gmail.com
+</li>
               <li>Australia</li>
             </ul>
 
